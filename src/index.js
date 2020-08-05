fe47f5986da3968ea1d87f85a4040c66c50f4507
joao.oliveira0117@hotmail.com <joao.oliveira0117@hotmail.com>
2020-08-04 22:38:43 -0300

    create get, put and delete roots

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ app.use(express.json())
 
 const scraps = [];
 
+app.get('/scraps',(request, response) => {
+    return response.json(scraps);
+})
 
 //create
-
 app.post('/scraps', (request, response) => {
     const {scrapTitle, scrapBody} = request.body;
 
-    const scrap = {scrapTitle, scrapBody};
+    const scrap = {id: uuid(),scrapTitle, scrapBody};
 
     scraps.push(scrap);
 
@@ -21,8 +23,40 @@ app.post('/scraps', (request, response) => {
 });
 
 //edit
+app.put(`/scraps/:id`, (request, response) => {
+  const { id } = request.params;
+  const { scrapTitle, scrapBody } = request.body;
+
+  const scrapsIndex = scraps.findIndex((scrap) => scrap.id === id);
+
+  if (scrapsIndex < 0) {
+    return response.status(400).json({ error: " Project not found" });
+  }
+  const scrap = {
+    id,
+    scrapTitle,
+    scrapBody,
+  };
+  scraps[scrapsIndex] = scrap;
+  return response.json(scrap);
+});
 
 //delete
+app.delete('/scraps/:id', (request, response) => {
+
+    const { id } = request.params;
+    const { scrapTitle, scrapBody } = request.params;
+    const scrapIndex = scraps.findIndex(scrap => scrap.id == id);
+    
+    if(scrapIndex < 0) {
+        return response.status(400).json({error: "Scrap not found"});
+    }
+
+    scraps.splice(scrapIndex, 1);
+
+    return response.status(204).send();
+})
+
 
 const port = 3333;
 app.listen(port, () => {

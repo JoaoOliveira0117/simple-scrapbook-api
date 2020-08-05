e69ab5af251d44731ea99e3a35c887a23f74098e
joao.oliveira0117@hotmail.com <joao.oliveira0117@hotmail.com>
2020-08-04 22:16:05 -0300

    create post root
    Co-authored-by: luisgdesouzac@gmail.com <luisgdesouzac@gmail.com>
    Co-authored-by: ticsoares@hotmail.com <ticsoares@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@ const {uuid} = require("uuidv4");
 
 const app = express();
 
+app.use(express.json())
+
+const scraps = [];
+
+
 //create
+
+app.post('/scraps', (request, response) => {
+    const {scrapTitle, scrapBody} = request.body;
+
+    const scrap = {scrapTitle, scrapBody};
+
+    scraps.push(scrap);
+
+    return response.json(scrap);
+});
 
 //edit
 
 //delete
+
+const port = 3333;
+app.listen(port, () => {
+  console.log(`👌 Server up and running on PORT ${port}`);
+});
